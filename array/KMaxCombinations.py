183e8f09f1d764142ef95ba6cd167dbda6228fe4
rounak31jp <rkagarwal310196@gmail.com>
2020-03-11 21:19:18 +0530

    Update KMaxCombinations.py

--- array/KMaxCombinations.py
+++ array/KMaxCombinations.py
@@ -1,25 +1,3 @@
-K maximum sum combinations from two arrays
-Given two equally sized arrays (A, B) and N (size of both arrays).
-A sum combination is made by adding one element from array A and another element of array B. 
-Display the maximum K valid sum combinations from all the possible sum combinations.
-
-Examples:
-
-Input :  A[] : {3, 2} 
-         B[] : {1, 4}
-         K : 2 [Number of maximum sum
-               combinations to be printed]
-Output : 7    // (A : 3) + (B : 4)
-         6    // (A : 2) + (B : 4)
-
-Input :  A[] : {4, 2, 5, 1}
-         B[] : {8, 0, 3, 5}
-         K : 3
-Output : 13   // (A : 5) + (B : 8)
-         12   // (A : 4) + (B :  8)
-         10   // (A : 2) + (B : 8) 
-         
-
 
 import math  
 from queue import PriorityQueue 
