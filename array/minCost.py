f0d590588dd138f97e3bd5126e1402bbcc2a31cb
rounak31jp <rkagarwal310196@gmail.com>
2020-03-11 21:16:00 +0530

    Update and rename Minimum cost to make all array elements equal.txt to minCost.py

--- array/minCost.py
+++ array/minCost.py
@@ -29,3 +29,24 @@ minimum of these two as the answer. This approach works because we can make all 
 is even. The elements from both the types can be changed into the other type with 1 unit cost and in order to minimise the cost, 
 the result will be the min(frequency(x), frequency(x + 1)).
 
+
+
+
+
+def minCost(arr, n): 
+
+    count_even = 0
+    count_odd = 0
+
+    for i in range(n): 
+        if (arr[i] % 2 == 0): 
+            count_even += 1
+        else: 
+            count_odd += 1
+  
+    return min(count_even, count_odd) 
+  
+arr = [2, 4, 3, 1, 5] 
+n = len(arr) 
+  
+print(minCost(arr, n)) 
