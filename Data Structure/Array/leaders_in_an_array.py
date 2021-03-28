da58a24c72b8f9ab14493a721a3e262cdfb5dd7f
rounak31jp <rkagarwal310196@gmail.com>
2021-03-28 12:20:56 +0530

    Update leaders_in_an_array.py

--- Data Structure/Array/leaders_in_an_array.py
+++ Data Structure/Array/leaders_in_an_array.py
@@ -13,3 +13,19 @@ def printLeaders(arr,size):
    
 arr = [16, 17, 4, 3, 5, 2] 
 printLeaders(arr, len(arr))
+
+
+# Method 2
+
+def printLeaders(arr, size):
+     
+    max_from_right = arr[size-1]   
+    print (max_from_right) 
+    
+    for i in range( size-2, -1, -1):        
+        if max_from_right <= arr[i]:        
+            print (arr[i])
+            max_from_right = arr[i]
+
+arr = [16, 17, 4, 3, 5, 2]
+printLeaders(arr, len(arr))
